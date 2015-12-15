de01fbba56e44a78b1b856bbcf4ffba09ed996d9
MrZura <katsura.kotaro@yandex.ru>
2015-12-15 09:39:00 +0400

    Finish user singup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Katsura Kotarou"
+    email    "katsura.kotaro@yandex.ru"
+    password "netunetu"
+    password_confirmation "netunetu"
+  end
+end
